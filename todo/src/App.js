39be69f97fc20bba36150883119f86ff036bc713
tirthshah147 <tirthshah147@gmail.com>
2021-05-04 23:16:37 +0530

    Added progress bar

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     this.state = {
       items: [],
       input: '',
-      isEditing: false
+      isEditing: false,
+      progress:0
     }
   }
   onEditing = () => {
@@ -19,17 +20,42 @@ class App extends Component {
   onSubmit = e => {
     e.preventDefault();
     const items = {id: new Date().getTime(), text : this.state.input, isCompleted:false};
-    this.setState({items : this.state.items.concat(items), input:""});
+    this.setState({items : this.state.items.concat(items), input:""},() => this.setProgress(this.state.items));
    }
 
   onClick = id => {
       console.log(id);
+      let updated = this.state.items.map(item => {
+        if(item.id === id){
+          item.isCompleted = !item.isCompleted;
+        }
+        return item;
+      })
+      this.setState({items : updated}, () => this.setProgress(this.state.items));
    }
 
-  onDelete = id => { }
+
+  onDelete = (id) => { 
+    let filtered = this.state.items.filter(item => {
+      return item.id != id;
+    })
+    this.setState({items : filtered}, () => this.setProgress(this.state.items));
+    // this.setProgress(this.state.items);
+  }
+
+  setProgress = items => {
+    let checked = items.filter(item => {return item.isCompleted})
+    let progress = checked.length/items.length
+    this.setState({progress : Math.floor(progress * 100)})
+  }
+
   render() {
     return (
       <form id="todo-list" onSubmit={this.onSubmit}>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" style={{width: this.state.progress + "%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{this.state.progress + "%"}</div>
+        </div> 
+
         { this.state.items.length > 0 ?
         this.state.items.map((item,i) => {
           return(
@@ -38,7 +64,8 @@ class App extends Component {
               <input type="checkbox" checked={item.isCompleted}/>
               <label className="todo" ><i className="fa fa-check"></i>{item.text}</label>
             </span>
-            <span className="delete-item" title="remove" >
+
+            <span className="delete-item" title="remove" onClick={() => this.onDelete(item.id)}>
               <i className="fa fa-times-circle"></i>
             </span> 
           </span>
@@ -49,6 +76,7 @@ class App extends Component {
         {this.state.isEditing ? <span className="todo-wrap" ><input onChange={this.onChange} value={this.state.input}/></span> : ""}
         
         <div id="add-todo" onClick={this.onEditing}><i className="fa fa-plus"></i> &nbsp; Add an Item</div>
+
       </form>
     );
   }
