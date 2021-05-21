0f5bac32a8c68c8ddbc15165f784e66168aaa70f
tirthshah147 <tirthshah147@gmail.com>
2021-05-21 23:13:57 +0530

    Learned Context API with application

---
+++ contextusecase/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeToggle from './components/ThemeToggle';
+import ThemeContextProvider from './context/ThemeContext';
+
+
+function App() {
+  return (
+    <div className="App">
+    <ThemeContextProvider>
+      <Navbar />
+      <BookList />
+      <ThemeToggle/>
+    </ThemeContextProvider>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ contextusecase/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {ThemeContext} from '../context/ThemeContext';
+
+class Navbar extends Component {
+  // static contextType = ThemeContext;
+  render() { 
+    // console.log(this.context);
+    // const {isLightTheme, light, dark} = this.context;
+    // const theme = isLightTheme ? light : dark;
+    return ( 
+      <ThemeContext.Consumer>
+      {
+        (context) => {
+          const {isLightTheme, light, dark} = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{background: theme.ui, color : theme.syntax}}>
+                <h1>Context App</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+            </nav>
+          )
+        }
+      }
+      </ThemeContext.Consumer>
+      
+    );
+  }
+}
+ 
+export default Navbar;
